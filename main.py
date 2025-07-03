be525eaa88afce728614cc2db9a3825204e1e895
NathanYinka <api@snaarp.com>
2025-07-03 08:35:18 -0700

    eedited

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],  # allow all methods
     allow_headers=["*"],  # allow all headers
+    expose_headers=["Content-Disposition"],
 )
 
 @app.post("/sign-pdf/")
